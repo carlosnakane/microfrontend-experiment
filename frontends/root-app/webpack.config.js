3bcfaaefa12f843db1b6c8d85f4efd861c5e7df9
carlosnakane <carlos.nakane@gmail.com>
2019-05-20 14:00:33 -0300

    root-app styles

--- frontends/root-app/webpack.config.js
+++ frontends/root-app/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -26,10 +29,15 @@ module.exports = {
     publicPath: '/root-app/'
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       inject: 'head',
       title: 'Root App',
       filename: path.resolve(__dirname, 'dist/index.html')
-    })
+    }),
+    new HtmlWebpackTagsPlugin({ tags: ['styles.css'], append: true }),
+    new CopyWebpackPlugin([
+      { from: path.resolve(__dirname, 'src/styles.css'), to: path.resolve(__dirname, 'dist/root-app/styles.css') },
+    ]),
   ]
 };
