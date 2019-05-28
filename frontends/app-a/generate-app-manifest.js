3b3840cb623438fadaccd33e52a9aa31ee085a4b
carlosnakane <carlos.nakane@gmail.com>
2019-05-28 15:35:32 -0300

    app-a manifest generator

---
+++ frontends/app-a/generate-app-manifest.js
@@ -0,0 +1,26 @@
+const path = require('path');
+var fs = require('fs');
+const CRAAssetManifest = require(path.resolve(__dirname, './build/asset-manifest.json'));
+
+const assets = Object.keys(CRAAssetManifest.files)
+  .filter(name => name.endsWith('.js') || name.endsWith('.css'))
+  .filter(name => name !== 'service-worker.js')
+  .filter(name => name !== 'main.js')
+  .filter(name => !name.match(/^precache-manifest\.[\w\d]+\.js$/))
+  .map(name => CRAAssetManifest.files[name]);
+
+const appManifest = {
+	"entrypoint": CRAAssetManifest.files['main.js'],
+	"components": [
+		"component-a"
+	],
+	"assets": assets
+};
+
+fs.writeFile (path.resolve(__dirname, "./build/app-manifest.json"), JSON.stringify(appManifest), function(err) {
+    if (err) {
+      throw err;
+    }
+    console.log('app-manifest.json written');
+  }
+);
